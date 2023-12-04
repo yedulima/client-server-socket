4ee4f10186f5da198e56453b6f13c96e5b03ffc5
Eduardo Lima <108648404+yedulima@users.noreply.github.com>
2023-12-04 05:29:31 -0300

    FEAT / Server

---
+++ Server.py
@@ -0,0 +1,71 @@
+import socket
+import threading
+from time import sleep
+import os
+
+class Server:
+    HOST: str = socket.gethostbyname(socket.gethostname())
+    PORT: int = 1024
+    ADDR: tuple = (HOST, PORT)
+
+    clients = []
+    messages = []
+
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def startServer(self):
+        clean_terminal()
+        print("[SERVER] Connected waiting for clients...")
+        self.server.bind(self.ADDR)
+        self.server.listen()
+        while True:
+            connection, address = self.server.accept()
+            Thread = threading.Thread(target=self.handleClient, args=(connection, address))
+            Thread.start()
+            print(f"[SERVER] {threading.activeCount() - 1} clients connected.")
+
+    @classmethod
+    def addNewClient(cls, port: int, name: str):
+        cls.clients.append({port: name})
+        cls.messages.append({port: None})
+
+    @classmethod
+    def replaceClientMessage(cls, port: int, message: str) -> None:
+        for client in range(len(cls.messages)):
+            if port in cls.messages[client]:
+                cls.messages[client][port] = message
+                return None
+
+    @classmethod
+    def sendChat2Client(cls, connection):
+        for client in range(len(cls.messages)):
+            client_port = [port for port in cls.messages[client]][0]
+            client_name = [cls.clients[name][client_port] for name in range(len(cls.clients)) if client_port in cls.clients[name]][0]
+            final_message = f"[{client_name}] {cls.messages[client][client_port]}"
+            connection.send(final_message.encode())
+            sleep(.2)
+
+    def handleClient(self, connection, address: tuple):
+        print(f"[SERVER] {address} conected to the server!")
+        client_name = connection.recv(64).decode()
+        self.addNewClient(address[1], client_name)
+        while True:
+            message_lenght = connection.recv(64).decode()
+            if message_lenght:
+                message_lenght = int(message_lenght)
+                message = connection.recv(message_lenght).decode()
+                if message == 'quit':
+                    break
+                self.replaceClientMessage(address[1], message)
+                self.sendChat2Client(connection)
+                print(f"[{address[1]}] {message}")
+        print(f"[SERVER] {[self.clients[client][address[1]] for client in range(len(self.clients)) if address[1] in self.clients[client]]} disconnected to the server.")
+        connection.close()
+
+if __name__ == "__main__":
+
+    clean_terminal = lambda: os.system("cls") if os.name == 'nt' else os.system("clear")
+
+    server = Server()
+    server.startServer()
